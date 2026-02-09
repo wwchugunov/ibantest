c4b2be121ac491c6c53e92042f1d16fa98edfcf0
wwchugunov <78274510+wwchugunov@users.noreply.github.com>
2026-02-09 14:14:54 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -62,7 +62,7 @@ if (isMobile) {
 }
 
 function openPayment() {
-  // window.open(paymentUrl, "_blank");
+  window.open(paymentUrl, "_blank");
   console.log(paymentUrl)
 }
 
@@ -83,16 +83,16 @@ function openBank(code) {
   if (ua.includes("android")) {
     link = paymentUrl.replace("https://", "intent://") +
       "#Intent;scheme=https;package=" + b.android + ";end";
-     // alert(${link})
+     alert(${link})
   }
 
   if (ua.includes("iphone")) {
     link = paymentUrl.replace("https://", b.ios + "://");
-     // alert(${link});
+     alert(${link});
     
   }
 
   // alert(${link})
-  // location.href = link;
-  console.log(link)
+  location.href = link;
+  // console.log(link)
 }
